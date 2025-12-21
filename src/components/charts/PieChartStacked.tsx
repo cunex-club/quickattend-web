b742dd9f09ee42ba9d2565fa49d0b2d7f6b0f958
Aluminium51 <noptanadon.p@gmail.com>
2025-12-21 09:27:41 +0700

    feature(charts) : add PieChartStacked component for displaying stacked pie charts.

---
+++ src/components/charts/PieChartStacked.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { cn } from "@assets/lib/utils";
+import { Pie, PieChart, Cell } from "recharts";
+
+import { Card, CardContent } from "@assets/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@assets/components/ui/chart";
+
+type PieChartStackedProps = {
+  category: string;
+  total: number;
+};
+
+const chartConfig = {
+  total: {
+    label: "Total",
+    color: "var(--chart-pink-500)",
+  },
+} satisfies ChartConfig;
+
+export function PieChartStacked({
+  dataInner,
+  dataOuter,
+}: {
+  dataInner: PieChartStackedProps[];
+  dataOuter: PieChartStackedProps[];
+}) {
+  const chartDataInner = dataInner.map((item, index) => ({
+    ...item,
+    fill: index === 0 ? "var(--color-primary)" : "var(--color-gray-100)",
+  }));
+
+  const chartDataOuter = dataOuter.map((item, index) => ({
+    ...item,
+    fill: index === 0 ? "var(--color-pink-200)" : "var(--color-neutral-white)",
+  }));
+
+  return (
+    <Card className="flex flex-col">
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className={cn(
+            "mx-auto aspect-square max-h-[300px]",
+            "chart-hover-pie"
+          )}
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent className="label-small-primary" />}
+            />
+            {/* inner pie */}
+            <Pie
+              data={chartDataInner}
+              outerRadius={140}
+              dataKey="total"
+              nameKey="category"
+              startAngle={90}
+              endAngle={-270}
+              strokeWidth={3}
+            >
+              {chartDataInner.map((entry, index) => (
+                <Cell key={`cell-inner-${index}`} />
+              ))}
+            </Pie>
+            {/* outer ring */}
+            <Pie
+              data={chartDataOuter}
+              dataKey="total"
+              startAngle={90}
+              endAngle={-270}
+              innerRadius={142}
+              outerRadius={149}
+              tooltipType="none"
+              strokeWidth={3}
+            >
+              {chartDataOuter.map((entry, index) => (
+                <Cell key={`cell-outer-${index}`} />
+              ))}
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
