da60caec38a3fd42437ca73fef011fa62dc59db0
BadLuckZ <thanagorn8802@gmail.com>
2026-07-07 17:09:13 +0700

    fix: eventDateTime

--- src/utils/eventDateTime.ts
+++ src/utils/eventDateTime.ts
@@ -24,9 +24,13 @@ export const formatEventTimeRange = (
   return `${start} - ${end}`;
 };
 
-export const formatHourBucket = (isoStr: string, locale: string) =>
-  new Date(isoStr).toLocaleTimeString(localeToIntlTag(locale), {
+export const formatHourBucket = (isoStr: string, locale: string) => {
+  const date = new Date(isoStr);
+  if (Number.isNaN(date.getTime())) return "-";
+
+  return date.toLocaleTimeString(localeToIntlTag(locale), {
     hour: "2-digit",
     minute: "2-digit",
     hour12: false,
   });
+};
